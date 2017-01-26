bac1e481ae5728ed7816da7263322f353d1fb716
Nate Anderson <curtislowe10@gmail.com>
2017-01-26 11:56:22 -0700

    add edit and update methods to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,24 @@ class MoviesController < ApplicationController
   def index
     @movies = current_user.movies.all
   end
+  
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  
+  def update 
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      flash[:error] = @movie.errors.full_messages.join(' ')
+      redirect_to edit_movie_path
+    end
+  end
+  
+  private 
+  
+  def movie_params
+    params.require(:movie).permit(:title, :note)
+  end
 end
