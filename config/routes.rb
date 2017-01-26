d5fc9e4556a76abd89cfcda352e86bb99062c1fb
Nate Anderson <curtislowe10@gmail.com>
2017-01-26 11:01:03 -0700

    add edit routes to movies

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "movies#index"
   
-  resources :movies, only: [:index]
+  resources :movies, only: [:index, :edit]
   
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
