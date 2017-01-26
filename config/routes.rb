ba2d9dec02da36cf8e88bac8c7d29feb1b2b4055
Nate Anderson <curtislowe10@gmail.com>
2017-01-26 10:01:32 -0700

    add routes for login, signup, and movies index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "movies#index"
+  
+  resources :movies, only: [:index]
+  
+  get '/login' => 'sessions#new', as: 'login'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new', as: 'signup'
+  post '/users' => 'users#create'
 end
