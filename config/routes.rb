c74d89c7c70d482c3aaf5397851b1629562aff4d
Nate Anderson <curtislowe10@gmail.com>
2017-01-26 13:18:44 -0700

    add update route for api movies

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :movies, only: [:create]
+      resources :movies, only: [:create, :update]
     end
   end
 end
